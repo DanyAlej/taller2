0dac78a4a57b32a536c0a20cfaee4c2ed71cb2ab
DanyAlej <da.benavides@uniandes.edu.co>
2020-08-30 18:03:45 -0500

    updated test

--- puppeteer/jest.config.js
+++ puppeteer/jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
     preset: "jest-puppeteer",
     globals: {
-        //URL: "https://angular-6-registration-login-example.stackblitz.io/register"
-        //URL: "https://losestudiantes.co/"
-
         URL: "http://localhost:4200/account/register"
     },
     testMatch: [
